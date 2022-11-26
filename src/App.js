a5eb6a208d07375813c53e8adb531b4109d5fe66
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-11-27 01:27:16 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
+const singers = [
+  {name : "bappi lahiri",job:'singing',bankBalance: '1200'},
+  {name : "manna dey",job:' older singing',bankBalance: '2200'},
+  {name : "Boka Jamai",job:'singing',bankBalance: '33020'},
+  {name : "vojon",job:'singing',bankBalance: '2100'}
+]
 
 function App() {
+  const nayoks = ['Hero Alam','Andrew kishore','Asif Akbar','Pori Babu']
   return (
     <div className="App">
+      {
+        singers.map(singer => <Person name={singer.name} job={singer.job} ></Person>)
+      }
+      {
+        // nayoks.map(nayok => <Person name={nayok}></Person>)
+      }
       {/* singer */}
-      <h1 className='allSingers'>all of famous singers of BD</h1>
-      <Person name ="Hero Alam"song="badam badam"></Person>
-      <Person name ="Andrew Kishore"song="amar Deho Tori"></Person>
-      <Person name ="Asif Akbar"song="O priya priya"></Person>
-      <Person name ="Pori Babu"song="hero alam fans"></Person>
+      {/* <Person name ={nayoks[0]} song="badam badam"></Person>
+      <Person name ={nayoks[1]} song="amar Deho Tori"></Person>
+      <Person name ={nayoks[2]} song="O priya priya"></Person>
+      <Person name ={nayoks[3]} song="hero alam fans"></Person> */}
       {/* player */}
-      <Player cricketerNo1 ="sakib hasan"six="100"></Player>
+      <Player cricketerNo1 ="sakib hasan"six="120"></Player>
+      <Player cricketerNo1 ="farhaz toni"six="100"></Player>
+      <Player cricketerNo1 ="minu "six="80"></Player>
     </div>
   );
 }
+
+
 // 1..to pass data into Component by 'Person'function name with props
 function Person(props){
   // console.log(props)
  return (
    <div className="person">  
-      <h2>song : {props.song}</h2>
+      <h2>song : {props.job}</h2>
       <p id='hello'>singer : {props.name}</p>
    </div>
   );
@@ -32,7 +48,7 @@ function Player(props){
   // console.log(props)
  return (
    <div className="person">  
-      <h2>BD  cricketer no : 1 is :{props.cricketerNo1} </h2>
+      <h2>BD cricketer is :{props.cricketerNo1} </h2>
       <p id='hello'>total Six : {props.six}</p>
    </div>
   );
