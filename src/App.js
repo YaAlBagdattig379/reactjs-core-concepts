75d0273a7c67d4991c801742734cd01c03d05313
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-11-26 12:17:52 +0530

    added

--- src/App.js
+++ src/App.js
@@ -4,21 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <h1>hero alam is a great actor in BD: <Person></Person></h1>
+      
     </div>
+  );
+}
+function Person(){
+ return (
+   <div className="person">  
+      <h1>Famous Singer of Hero Alam </h1>
+      <p id='hello'>Hero alam</p>
+   </div>
   );
 }
 
